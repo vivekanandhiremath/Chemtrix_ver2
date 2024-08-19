652f0467cba6e1e75490553c5009d66adb2e0580
vivekanand <vivekanand293@gmail.com>
2024-08-19 18:13:18 +0530

    VisitPlanning

--- src/test/java/com/qa/stepdef/StepsDef.java
+++ src/test/java/com/qa/stepdef/StepsDef.java
@@ -2,6 +2,7 @@ package com.qa.stepdef;
 
 import com.qa.base.WebdriverManager;
 import com.qa.pageobj.LoginPage;
+import com.qa.pageobj.VisitPlanPage;
 import com.qa.utils.ConfigReader;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -13,6 +14,7 @@ public class StepsDef {
     private Properties prop;
     private WebDriver driver;
     private LoginPage lp;
+    private VisitPlanPage vp;
 
     @Given("user is on login page")
     public void userIsOnLoginPage() {
@@ -22,6 +24,8 @@ public class StepsDef {
         // Initialize the WebDriver using the browser property
         driver = WebdriverManager.getInstance(prop.getProperty("browser")).getDriver();
         lp = new LoginPage(driver);
+
+        vp = new VisitPlanPage(driver);
 
     }
 
@@ -33,5 +37,29 @@ public class StepsDef {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Then("user enter username as {string} and password as {string}")
+    public void userEnterUsernameAsAndPasswordAs(String username, String password) {
+//       Login login= new Login.LoginBuilder().setUsername(username).setPassword(password).build();
+//
+        lp.enterUserName(username);
+        lp.enterPassword(password);
+        lp.clickOnLogin();
+    }
+
+
+    @Then("user navigates to visit planning dashboard and click on create vsisit planning button")
+    public void userNavigatesToVisitPlanningDashboardAndClickOnCreateVsisitPlanningButton() {
+
+        vp.clickOnMyActivity();
+        vp.clickOnVisitPlanning();
+        vp.clickOnCreateVisitPlanning();
+
+    }
+
+    @Then("^user fills the visit planning details with customer type (.*) customer name (.*), phone number (.*), email (.*), address (.*), visit date (.*), visit time (.*), purpose of visit (.*), and remarks (.*)$")
+    public void userFillsTheVisitPlanningDetailsWithCustomerTypeCustomertypeCustomerNameNamePhoneNumberPhnoEmailEmailAddressAddressVisitDateVisitdateVisitTimeVisisttimePurposeOfVisitPurposeAndRemarksRemarks(String customertype,String customername, String phno, String email, String address, String visitdate, String visittime, String purpose, String remarks) {
+        vp.entervisitPlanningdetails();
     }
 }
